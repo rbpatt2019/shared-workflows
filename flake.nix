dc9551e7836a3ac478eccb4c000ec0bea63e861b
Ryan Patterson-Cross <43184541+rbpatt2019@users.noreply.github.com>
2026-06-16 23:18:02 +0100

    Update workflow formats (#13)
    * build(flake.lock): update dependencies
    
    * refactor(flake.nix): update structure
    
    * ci(check_nix.yaml): update nix install action
    
    * ci(update_nix.yaml): use quick install

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     git-hooks-nix.url = "github:cachix/git-hooks.nix";
-    devenv.url = "github:cachix/devenv";
   };
 
   outputs =
@@ -23,48 +22,41 @@
         imports = [
           inputs.treefmt-nix.flakeModule
           inputs.git-hooks-nix.flakeModule
-          inputs.devenv.flakeModule
         ];
         systems = nixpkgs.lib.systems.flakeExposed;
         perSystem =
-          { config, pkgs, ... }:
+          { config, ... }:
           {
-            devShells.default = pkgs.mkShell {
-              shellHook = ''
-                ${config.pre-commit.installationScript}
-                echo 1>&2 "Welcome to the development shell!"
-              '';
-            };
-            pre-commit = {
-              check.enable = false; # All non-trivial checks handled by treefmt
-              settings.hooks = {
-                check-added-large-files.enable = true;
-                trim-trailing-whitespace.enable = true;
-                end-of-file-fixer.enable = true;
-                treefmt = {
-                  enable = true;
-                  verbose = true;
-                  settings.formatters = [
-                    pkgs.actionlint
-                    pkgs.nixfmt-pkgs
-                    pkgs.deadnix
-                    pkgs.statix
-                    pkgs.rstfmt
-                    pkgs.yamlfmt
-                  ];
-                };
-              };
-            };
             treefmt = {
+              flakeCheck = true;
+              flakeFormatter = true;
               programs = {
                 actionlint.enable = true;
                 nixfmt.enable = true;
-                deadnix.enable = true;
                 statix.enable = true;
+                deadnix.enable = true;
                 rstfmt.enable = true;
                 yamlfmt.enable = true;
               };
             };
+            pre-commit.settings.hooks = {
+              check-added-large-files.enable = true;
+              check-merge-conflicts.enable = true;
+              end-of-file-fixer.enable = true;
+              mixed-line-endings.enable = true;
+              trim-trailing-whitespace.enable = true;
+              forbid-submodules = {
+                enable = true;
+                name = "Forbid git submodules";
+                description = "Forbids all git submodules in current dir.";
+                language = "fail";
+                entry = "Git submodules are not allowed here: ";
+                types = [ "directory" ];
+              };
+              treefmt.enable = true;
+              flake-checker.enable = true;
+            };
+            devShells.default = config.pre-commit.devShell;
           };
       }
     );
